2af08f1c9af71759a5da39387cc23f939c844b45
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-05-17 15:04:58 +0900

    Update.

--- spec/mur_spec.rb
+++ spec/mur_spec.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
-lib = File.expand_path('lib', __dir__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
 ENV['RACK_ENV'] = 'test'
 
-require 'stand'
+require './stand'
 require 'spec_helper'
 require 'rspec'
 require 'rack/test'
