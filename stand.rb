80bfd329ad9ec9fc4c25ab5358e2ff5ca1fbd026
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-05-17 12:54:23 +0900

    Update.

--- stand.rb
+++ stand.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require 'sinatra'
 require 'feedjira'
 require 'date'
 require 'sinatra/reloader'
 require 'httparty'
-#require "#{File.dirname(__FILE__)}/lib/himekuri"
-# require 'himekuri'
+require 'himekuri'
+require 'zella'
 
 # class katana is mumei and slogan
 class Katana
@@ -22,13 +25,12 @@ get '/' do
   @kanban = '[これより、30タイトル程ずつ履歴表示します]'
   url = 'http://github.com/takkii.atom'
   @rss = HTTParty.get(url).body
-  # Feedjira 2.2.0
-  # @rss = Feedjira::Feed.fetch_and_parse('http://github.com/takkii.atom')
   @ruby = ('Ruby_VERSION : ' + RUBY_VERSION + ' ' + 'Sinatra_VERSION : ' + Sinatra::VERSION)
-  # @himekuri = HimekuriClass.new.himekuri
-  # @reiwa = HimekuriClass.new.reiwa
+  @himekuri = HimekuriClass.new.himekuri
+  @reiwa = HimekuriClass.new.reiwa
   @year = DateTime.now.year
-  # @wahugetsu = HimekuriClass.new.wahugetsu_web
+  @wahugetsu = HimekuriClass.new.wahugetsu_web
+  @zella = Zella.formula
   @takkii = ' Takayuki Kamiyama All Rights Reserved. '
   @saya = Katana.new('Designed by NyaSoCom', 'Not Yet Another Software design of Computer')
   @nyasocom_version = '1.1'
