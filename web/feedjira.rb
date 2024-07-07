47b9daa66818857c72b0744aee59ffffe35810e3
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-07-07 17:16:46 +0900

    Update.

--- web/feedjira.rb
+++ web/feedjira.rb
@@ -9,7 +9,7 @@ begin
 
   Feedjira.parse(@rss).entries.each do |entry|
     File.open('rss.xml', 'a:utf-8', perm = 0o777) do |f|
-      f.puts "#{entry.summary} <a href='#{entry.url}'> #{entry.title}</a>"
+      f.puts "#{entry.summary} <a href='#{entry.url}'>#{entry.title}</a>"
       f.puts ''
       f.puts entry.published.to_time.strftime '投稿時刻 ： %Y年%m月%d日 %H時%M分'
       f.puts ''
